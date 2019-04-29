558cd2aab5dd90371836dadd1b8017825e506db4
HugoCapocci <h.capocci@gmail.com>
2019-04-29 15:18:13 +0200

    Implements simple message send/receive

--- web/websocket-server.js
+++ web/websocket-server.js
@@ -5,6 +5,7 @@ const webSocketServer = new WebSocket.Server({ port: 3001 });
 webSocketServer.on('connection', function connection(webSocket) {
   webSocket.on('message', function incoming(message) {
     console.log('received: %s', message);
+    webSocket.send(message);
   });
-  webSocket.send('something');
+  webSocket.send('Welcome!');
 });
